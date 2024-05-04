ea6bddfc6c18aeb182617aec4e6a07d0e4d0ff5e
Afrimo <Afrimthaqi1401@yahoo.no>
2024-05-04 19:54:49 +0200

    Login fully structured

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import "../styles/Header.css";
 
 export function Header() {
-  return <div className="main-header"></div>;
+  return (
+    <div className="main-header">
+      <img className="logo" src="images/fmt_logo.jpeg" />
+    </div>
+  );
 }
